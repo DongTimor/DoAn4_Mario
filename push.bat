02def97fdd6a28ad05cc65259828e9e1414785c5
DongTimor <nkdong.20it4@vku.udn.vn>
2022-12-03 21:55:01 +0700

    change

--- push.bat
+++
@@ -1,7 +0,0 @@
-echo "# DoAn4_Mario" >> README.md
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/DongTimor/DoAn4_Mario.git
-git push -u origin main
